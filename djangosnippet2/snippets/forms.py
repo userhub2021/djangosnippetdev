2c7d625b3d21ae1949f05700511b827247b6033d
Takahiro <88309900+userhub2021@users.noreply.github.com>
2022-02-28 15:59:54 +0900

    all new project

---
+++ djangosnippet2/snippets/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from django import forms
+
+from snippets.models import Snippet
+
+
+class SnippetForm(forms.ModelForm):
+    class Meta:
+        model = Snippet
+        fields = ('title', 'code', 'description')
